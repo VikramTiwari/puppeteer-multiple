6e9ee6a26350289ba783466b0ec304bc7d398c7c
Vikram Tiwari <vikramtheone1@gmail.com>
2018-10-31 15:24:29 -0700

    sample for headless which works

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const puppeteer = require('puppeteer')
 
 async function launch() {
-	const launchOptions = { headless: false, userDataDir: './tmp' }
+	// const launchOptions = { headless: true, userDataDir: './tmp' } // works
+	const launchOptions = { headless: false, userDataDir: './tmp' } // doesn't work
 
 	await puppeteer.launch(launchOptions)
 	await puppeteer.launch(launchOptions)
 	await puppeteer.launch(launchOptions)
+
+	console.log('all good!')
 }
 
 launch()
